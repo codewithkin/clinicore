80e75ef43d89658ea994b985af1ab2ab3b298d64
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-12-21 21:14:29 +0200

    feat: implement Collapsible and Select components for UI enhancement

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,10 +1,5 @@
 import { createAuthClient } from "better-auth/react";
-import { polarClient } from "@polar-sh/better-auth";
-import { organizationClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
-	plugins: [
-		polarClient(),
-		organizationClient() 
-	],
+	plugins: [],
 });
